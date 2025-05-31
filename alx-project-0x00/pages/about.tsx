d84518c523772a7d2f45bdc9b7fd66fee1642dcb
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-31 13:28:40 -0500

    Update Task

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,9 @@
+const About: React.FC = () => {
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <h1 className="text-x1 fonr-extralight"> About Page</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,11 @@
+const Landing: React.FC = () => {
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <h1 className="text-x1 font-extralight">
+                Landing Page
+            </h1>
+        </div>
+    )
+}
+
+export default Landing

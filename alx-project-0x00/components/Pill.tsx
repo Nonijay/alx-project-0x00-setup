ed50e589333fd293e4c09b6ff19a1ccfa8134232
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-31 19:54:00 -0500

    update task

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -3,7 +3,7 @@ import { PillProps } from "@/interfaces";
 const Pill: React.FC<PillProps> = ({title}) => {
     return (
         <div className="flex justify-center items-center bg-[#f9f9f9] px-3 w-auto h-[27px] rounded-full">
-            <p className="text-sm"> {title}</p>
+            <p className="text-sm text-black"> {title}</p>
         </div>
     )
 }

1d6cf4b912f15f950c5d8d3ebe01aee31e9d0fac
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-31 19:22:49 -0500

    update task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,4 +1,4 @@
-import Card from "@/components/Card";
+import Card from "@/components/Card"
 
 const Landing: React.FC = () => {
     return (
@@ -8,7 +8,7 @@ const Landing: React.FC = () => {
             </h1>
 
             <Card />
-            
+
         </div>
     )
 }

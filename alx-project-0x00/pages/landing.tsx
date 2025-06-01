9db2e3784818d825a3abf573f62a73983755edcf
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-31 19:13:46 -0500

    update task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,12 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
     return (
         <div className="flex flex-col h-screen justify-center items-center">
             <h1 className="text-x1 font-extralight">
                 Landing Page
             </h1>
+            <Card />
         </div>
     )
 }

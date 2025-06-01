a8fc89cc943bb9c7ab90e2655a07b6d09d452309
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-05-31 21:00:25 -0500

    update task

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -5,7 +5,9 @@ import React from "react";
 const Button: React.FC<ButtonProps> = ({title, styles}) => {
     return (
         <div className="h-[500px] w-[300px]">
-            <button className="sm:min-w-sm sm:w-sm sm:m-auto md:w-md lg:w-lg bg-blue-500 hover:bg-sky-500 text-white text-1 px-6 py-2 rounded-sm">{title}</button>
+            <button className="sm:min-w-sm sm:w-sm sm:m-auto md:w-md lg:w-lg bg-blue-500 hover:bg-sky-500 text-white text-1 px-6 py-2 rounded-sm">
+                {title}
+            </button>
         </div>
     )
 }
